d1cad79d39c22a8c76f18520da64c6faaffb86df
Birhane <Birhanegebrial1@gmail.com>
2022-10-06 11:10:36 +0300

    Some Change

--- app/Http/Controllers/softwareController.php
+++ app/Http/Controllers/softwareController.php
@@ -9,7 +9,6 @@ class softwareController extends Controller
 {
     function Browser($id=null)
     {
-
         $token = "e06a8fd22e0ffc40705f87610d1d5cd6";
         $idsite = 1;
         $period = "day";
@@ -17,11 +16,21 @@ class softwareController extends Controller
         $format = "json";
 
         $Browser_data = Http::get("http://localhost/matomo/index.php?module=API&method=DevicesDetection.getBrowsers&idSite={$idsite}&period={$period}&date={$date}&format={$format}&token_auth={$token}&force_api_session=1");
-        // $Browser = new Browser();
-        // $Browser->Browser_name = $Browser_data->label;
+        
+        $Browser = new Browser();
+        $Browser->Browser_name = $Browser_data[0]["label"];
+        $Browser->nb_uniq_visitors = $Browser_data[0]["nb_uniq_visitors"];
+        $Browser->nb_visits = $Browser_data[0]["nb_visits"];
+        $Browser->nb_actions = $Browser_data[0]["nb_actions"];
+        $Browser->nb_users = $Browser_data[0]["nb_users"];
+        $Browser->sum_visit_length = $Browser_data[0]["sum_visit_length"];
+        $Browser->bounce_count = $Browser_data[0]["bounce_count"];;
+        $Browser->nb_visits_convertes = $Browser_data[0]["nb_visits_converted"];
+        $Browser->logo = $Browser_data[0]["logo"];
+        $Browser->segment = $Browser_data[0]["segment"];
+        $Browser->save();
+
         return $Browser_data->json();
-
-
         // return $id?Browser::find($id):Browser::all();
     }
 }
--- app/Models/Browser.php
+++ app/Models/Browser.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Browser extends Model
 {
+    public $timestamps = false;
+    
     use HasFactory;
 }
