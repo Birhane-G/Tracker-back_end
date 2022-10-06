00a2aac981f7a5ed85876c993b02d85bcd26eefa
Birhane <Birhanegebrial1@gmail.com>
2022-10-06 12:27:17 +0300

    Software controller

--- app/Http/Controllers/softwareController.php
+++ app/Http/Controllers/softwareController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\Browser;
+
 class softwareController extends Controller
 {
     function Browser($id=null)
@@ -15,22 +16,29 @@ class softwareController extends Controller
         $date = "today";
         $format = "json";
 
-        $Browser_data = Http::get("http://localhost/matomo/index.php?module=API&method=DevicesDetection.getBrowsers&idSite={$idsite}&period={$period}&date={$date}&format={$format}&token_auth={$token}&force_api_session=1");
+        // $Browser_data = Http::get("http://localhost/matomo/index.php?module=API&method=DevicesDetection.getBrowsers&idSite={$idsite}&period={$period}&date={$date}&format={$format}&token_auth={$token}&force_api_session=1");
         
-        $Browser = new Browser();
-        $Browser->Browser_name = $Browser_data[0]["label"];
-        $Browser->nb_uniq_visitors = $Browser_data[0]["nb_uniq_visitors"];
-        $Browser->nb_visits = $Browser_data[0]["nb_visits"];
-        $Browser->nb_actions = $Browser_data[0]["nb_actions"];
-        $Browser->nb_users = $Browser_data[0]["nb_users"];
-        $Browser->sum_visit_length = $Browser_data[0]["sum_visit_length"];
-        $Browser->bounce_count = $Browser_data[0]["bounce_count"];;
-        $Browser->nb_visits_convertes = $Browser_data[0]["nb_visits_converted"];
-        $Browser->logo = $Browser_data[0]["logo"];
-        $Browser->segment = $Browser_data[0]["segment"];
-        $Browser->save();
+        $items = array(1,2,3,4,5,6,7,8);
 
-        return $Browser_data->json();
+       foreach($items as $item){
+        echo $item;
+       }
+      
+
+        // $Browser = new Browser();
+        // $Browser->Browser_name = $Browser_data[0]["label"];
+        // $Browser->nb_uniq_visitors = $data["nb_uniq_visitors"];
+        // $Browser->nb_visits = $data["nb_visits"];
+        // $Browser->nb_actions = $data["nb_actions"];
+        // $Browser->nb_users = $data["nb_users"];
+        // $Browser->sum_visit_length = $data["sum_visit_length"];
+        // $Browser->bounce_count = $data["bounce_count"];
+        // $Browser->nb_visits_convertes = $data["nb_visits_converted"];
+        // $Browser->logo = $data["logo"];
+        // $Browser->segment = $data["segment"];
+        // $Browser->save();
+
+        // return $data;
         // return $id?Browser::find($id):Browser::all();
     }
 }
